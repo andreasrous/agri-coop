2fad9293d4d19f4a2c9cfc49796e3249d2e82a61
Andreas Rousounelos <andreas.rousounelos@gmail.com>
2024-05-19 17:28:41 +0300

    added header

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -2,12 +2,19 @@
 import { RouterLink } from 'vue-router';
 import { useApplicationStore } from '@/stores/application.js';
 const applicationStore = useApplicationStore();
+function openMenu() {
+  const header = document.querySelector('header');
+  header.classList.toggle('open');
+}
 </script>
 
 <template>
   <header>
-    <div>
+    <div class="logo">
+      <img src="../assets/logo.svg">
       <a href="/"><span>agricoop</span></a>
+    </div>
+    <nav>
       <ul>
         <li v-if="applicationStore.isAuthenticated === true">
           <router-link :to="{ name: 'home' }">Home</router-link>
@@ -21,13 +28,14 @@ const applicationStore = useApplicationStore();
         <li v-if="applicationStore.isAuthenticated === true">
           <router-link :to="{ name: 'cultivation-locations' }">Cultivation Locations</router-link>
         </li> -->
-        <li v-if="applicationStore.isAuthenticated === false">
-          <router-link :to="{ name: 'login' }">Login</router-link>
-        </li>
-        <li v-if="applicationStore.isAuthenticated === true">
-          <router-link :to="{ name: 'logout' }">Logout</router-link>
-        </li>
       </ul>
+    </nav>
+    <div class="auth">
+      <router-link v-if="applicationStore.isAuthenticated === false" :to="{ name: 'login' }">Login</router-link>
+      <router-link v-if="applicationStore.isAuthenticated === true" :to="{ name: 'logout' }">Logout</router-link>
     </div>
+    <button id="menuButton" @click="openMenu">
+      <i class="bx bx-menu"></i>
+    </button>
   </header>
 </template>
